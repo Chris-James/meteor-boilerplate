b31afe45e92dcb05561480538cf825dd63859bb0
Chris James <chris@chris-james.me>
2017-06-28 20:59:13 -0400

    startup/accounts-hooks: Tidy onCreateUser documentation

--- imports/startup/server/accounts-hooks.js
+++ imports/startup/server/accounts-hooks.js
@@ -1,18 +1,20 @@
 import { Accounts } from 'meteor/accounts-base';
 
-/**
- * @method onCreateUser
- * @desc Called _server-side_ whenever a new user is created. Allows programmatic
- *  control of the contents of new user documents.
- *  http://docs.meteor.com/api/accounts-multi.html#AccountsServer-onCreateUser
+/*******************************************************************************
+ * onCreateUser
+ * Programatically control the content of a new user's database document before
+ * it's inserted into `Meteor.users`.
  *
- * @param  {Object} options The information gathered/generated during sign up process.
- * @param  {Object} options.profile Contains info gathered from useraccounts forms
- *  and/or information generated within useraccounts hooks.
- * @param  {Object} user  Created on server. A proposed user object that contains all
- *  automatically generated fields required for the user to log in, including `_id`.
+ * onCreateUser is called _server-side_ whenever a new user is created.
+ * @see {@link http://docs.meteor.com/api/accounts-multi.html#AccountsServer-onCreateUser}
  *
- * @return {Object} user The finalized document to be inserted in Meteor.users collection.
+ * @param {obj} options The information gathered/generated during sign up process.
+ * @param {obj} options.profile Contains info gathered from useraccounts forms
+ * and/or information generated within useraccounts hooks.
+ * @param {obj} user Created on server. A proposed user object that contains all
+ * automatically generated fields required for the user to log in, including `_id`.
+ *
+ * @return {obj} user The finalized document to be inserted in Meteor.users collection.
  */
 Accounts.onCreateUser((options, user) => {
 
