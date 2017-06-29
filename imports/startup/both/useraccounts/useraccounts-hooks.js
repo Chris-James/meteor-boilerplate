78672573c27b66f820341539abf418f001b0b436
Chris James <chris@chris-james.me>
2017-06-28 20:53:18 -0400

    startup/useraccounts-hooks: Tidy onLogout docs

--- imports/startup/both/useraccounts/useraccounts-hooks.js
+++ imports/startup/both/useraccounts/useraccounts-hooks.js
@@ -22,10 +22,9 @@
 
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
-/**
- * @function onLogout
- * @desc Invoked upon execution of `AccountsTemplates.logout()`. Allows for custom
- *  redirects and/or other actions to be executed upon user logout.
+/*******************************************************************************
+ * onLogout
+ * Redirects user to sign in page upon logout.
  */
 export const onLogout = function onLogout() {
   FlowRouter.go('/sign-in');
