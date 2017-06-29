7ec971e54b0294fb8ed22e4ee7fc255af6c5e58f
Chris James <chris@chris-james.me>
2017-06-28 20:50:13 -0400

    startup/useraccounts-fields: Add documentation

--- imports/startup/both/useraccounts/useraccounts-fields.js
+++ imports/startup/both/useraccounts/useraccounts-fields.js
@@ -1,3 +1,22 @@
+/*******************************************************************************
+ * @file useraccounts-fields
+ * @overview Allows programmatic control over the input fields (or "controls")
+ * included in the sign up form. This "programmatic control" encompasses what
+ * fields are included or excluded from the form, field types, field styles,
+ * field content (placeholders, labels, etc), if & how input is validated,
+ * and more.
+ *
+ * Custom fields are added via `AccountsTemplates.addField()` and default fields
+ * can be removed with calls to `AccountsTemplates.removeField()`. Removing default
+ * fields is less common but necessary in order to override default settings.
+ * @see {@link https://github.com/meteor-useraccounts/core/blob/master/Guide.md#add-a-field|UserAccounts: Adding/Removing Form Fields}
+ *
+ * Individual input fields are controlled via the properties passed into the
+ * `AccountsTemplates.addField()` method. An exhaustive list of field properties
+ * can be found in the UserAccounts documentation guide.
+ * @see {@link https://github.com/meteor-useraccounts/core/blob/master/Guide.md#form-fields-configuration|UserAccounts: Form Fields Configuration}
+ */
+
 import { AccountsTemplates } from 'meteor/useraccounts:core';
 
 // Remove default input fields provided by UserAccounts
