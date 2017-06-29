cf25167b0fda53073c69e81f73574ee90dc54665
Chris James <chris@chris-james.me>
2017-06-28 20:54:19 -0400

    users/publications: Tune 'currentUser'

--- imports/collections/users/server/publications.js
+++ imports/collections/users/server/publications.js
@@ -1,14 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { check } from 'meteor/check';
 
-Meteor.publish('currentUser.fields', function(fields) {
+/*******************************************************************************
+ * currentUser
+ * Publishes some or all of the current user's Mongo document.
+ *
+ * @param {arr|str} [fields] The specific fields of the document to publish. If
+ * no fields are specified the publication will return the entire document.
+ */
+Meteor.publish('currentUser', function(fields) {
 
-  check(fields, Array);
+  check(fields, Match.OneOf(String, Array, undefined));
 
+  // Iteratively add each field to an object that will be used as the projection.
+  // see: https://docs.mongodb.com/manual/reference/method/db.collection.find/#find-projection
   const filter = {};
-  fields.forEach(function(item) {
-    filter[item] = 1;
-  });
+
+  if (fields) {
+    if (typeof fields === "string") {
+      filter[fields] = 1;
+    } else {
+      fields.forEach(item => filter[item] = 1);
+    }
+  }
 
   return Meteor.users.find({
     "_id": this.userId
